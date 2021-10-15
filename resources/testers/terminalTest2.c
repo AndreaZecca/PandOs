20dd33cd370ebf88ea2bb5806eaab5958befd3e7
AndreaZecca <44266797+AndreaZecca@users.noreply.github.com>
2021-10-15 17:15:30 +0200

    File for testing PandOs

---
+++ resources/testers/terminalTest2.c
@@ -0,0 +1,15 @@
+/* Does nothing but outputs to the printer and terminates */
+
+#include <umps3/umps/libumps.h>
+
+
+#include "h/tconst.h"
+#include "h/print.h"
+
+void main() {
+	print(WRITETERMINAL, "printTest is ok\n");
+	
+	print(WRITETERMINAL, "Test Number 2 is ok\n");
+	
+	SYSCALL(TERMINATE, 0, 0, 0);
+}
